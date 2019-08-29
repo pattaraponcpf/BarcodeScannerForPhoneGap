bb42668837b678b7d4c7c0295c5a209768b59653
PATTARAPON BAMPEN <pattarapon.bam@cpf.co.th>
2019-08-29 16:34:09 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+#BarcodeScannerForPhoneGap
+
+The BarcodeScanner widget enables PhoneGap native barcode scanning functionality within your Mendix mobile application. This is a widget that will be functional from Mendix 7.13.1.
+
+## Add plugin
+When Publishing this App for Mobile App Stores, add in the the Custom Phonegap/Cordova configuration the barcode plugin
+``` xml
+<plugin name="phonegap-plugin-barcodescanner" />
+<edit-config target="NSCameraUsageDescription" file="*-Info.plist" mode="merge">
+    <string>To scan barcodes</string>
+</edit-config>
+```
+
+## Configuration
+
+Place the widget in a dataview where you want the button to be placed. Make sure this form is reachable from a mobile application.
+
+### Button
+#### Label
+The label text that is shown on the button.
+
+#### Class
+An optional class to be placed directly on the button dom node.
+
+### Data source
+#### Attribute
+The attribute on the dataview object where the resulting string should be set to.
+
+### Events
+#### On change microflow
+An optional microflow that will be triggered once the location has been retrieved.
+#### On change nanoflow
+An optional nanoflow that will be triggered once the location has been retrieved.
+
+## Contributing
+For more information on contributing to this repository visit [Contributing to a GitHub repository]( https://docs.mendix.com/howto/collaboration-requirements-management/contribute-to-a-github-repository)!
---
+++ src/BarcodeScannerForPhoneGap/widget/BarcodeScannerForPhoneGap.js
@@ -0,0 +1,121 @@
+/*global mxui, mx, dojo, cordova */
+define([
+    "mxui/widget/_WidgetBase", 
+    "mxui/dom", 
+    "dojo/dom-class", 
+    "dijit/_TemplateMixin", 
+    "dojo/_base/declare",
+    "dojo/Text!BarcodeScannerForPhoneGap/widget/template/BarcodeScannerForPhoneGap.html"
+], function(
+    _WidgetBase, 
+    mxuiDom, 
+    dojoClass, 
+    declare, 
+    _TemplateMixin, 
+    widgetTemplate) {
+    "use strict";
+
+    return declare("BarcodeScannerForPhoneGap.widget.BarcodeScannerForPhoneGap", [_WidgetBase, _TemplateMixin], {
+
+        templateString: widgetTemplate,
+
+        attributeName: "",
+        buttonLabel: "",
+        buttonClass: "",
+        onchangemf: "",
+        onChangeNanoflow: "",
+
+        _hasStarted: false,
+        _obj: null,
+        _button: null,
+
+        startup: function() {
+            if (this._hasStarted) return;
+
+            this._hasStarted = true;
+
+            dojoClass.add(this.domNode, "wx-barcodescanner-container");
+
+            this._createChildNodes();
+            this._setupEvents();
+        },
+
+        update: function (obj, callback) {
+            this._obj = obj;
+            if (callback) callback();
+        },
+
+        _setupEvents: function() {
+            var barcodeScannerAvailable = (
+                typeof(cordova) !== "undefined" &&
+                typeof(cordova.plugins.barcodeScanner) !== "undefined"
+            );
+
+            if (barcodeScannerAvailable) {
+                this.connect(this._button, "click", function() {
+                    cordova.plugins.barcodeScanner.scan(
+                        this._barcodeSuccess.bind(this),
+                        this._barcodeFailure.bind(this)
+                    );
+                });
+            } else {
+                this.connect(this._button, "click", function() {
+                    mx.ui.error("Unable to detect camera.");
+                });
+            }
+        },
+
+        _barcodeSuccess: function(output) {
+            if (!output.cancelled && output.text && output.text.length > 0) {
+                this._obj.set(this.attributeName, output.text);
+                this._executeAction();
+            }
+        },
+
+        _barcodeFailure: function(error) {
+            mx.ui.error("Scanning failed: " + error);
+        },
+
+        _executeAction: function () {
+            if (this.onchangemf && this._obj) {
+                var microflow = this.onchangemf;
+                mx.data.action({
+                    params: {
+                        actionname: microflow,
+                        applyto: "selection",
+                        guids: [ this._obj.getGuid() ]
+                    },
+                    origin: this.mxform,
+                    error: function (error) {
+                        mx.ui.error("An error occurred while executing microflow " + microflow + " : " + error.message);
+                    }
+                });
+            }
+            
+            if (this.onChangeNanoflow.nanoflow && this.mxcontext) {
+                mx.data.callNanoflow({
+                    nanoflow: this.onChangeNanoflow,
+                    origin: this.mxform,
+                    context: this.mxcontext,
+                    error: function (error) {
+                        mx.ui.error("An error occurred while executing the on change nanoflow: " + error.message);
+                    }
+                });
+            }
+        },
+
+        _createChildNodes: function() {
+            this._button = mxuiDom.create("div", {
+                "class": "wx-mxwxbarcodescanner-button btn btn-primary"
+            });
+            if (this.buttonClass) dojoClass.add(this._button, this.buttonClass);
+
+            this._button.textContent = this.buttonLabel || "Scan barcode";
+
+            this.domNode.appendChild(this._button);
+        }
+    });
+});
+
+// Compatibility with older mendix versions.
+require([ "BarcodeScannerForPhoneGap/widget/BarcodeScannerForPhoneGap" ], function() {});
